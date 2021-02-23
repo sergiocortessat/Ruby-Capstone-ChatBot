50a30f32f2b31398b5bf6871a4a60bbf253eda7a
Sergio <sbouden16@gmail.com>
2021-02-23 18:48:31 +0100

    Add set token.rb to receive token

--- token.rb
+++ token.rb
@@ -2,5 +2,5 @@ TOKEN = {
   'chatbot_name' => 'optional',
   'chatbot_username' => 'optional',
   'chatbot_link' => 'optional',
-  'token_key' => '1565232960:AAEH6D-6Te-C7MS3j8e-OEjXyvnzKkM4KN4'
+  'token_key' => 'OLBIGATORY'
 }.freeze
