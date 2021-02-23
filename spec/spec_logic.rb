0a34d8c577ad59945c56334eb0afe3f1081618e9
Sergio <sbouden16@gmail.com>
2021-02-23 15:57:17 +0100

    Add rspec test cases for motivational.rb

--- spec/spec_logic.rb
+++ spec/spec_logic.rb
@@ -2,38 +2,35 @@ require_relative '../lib/motivational'
 require_relative '../lib/bot_logic'
 require_relative '../lib/jokes'
 
-describe Quotes do
-  describe '#random_quote' do
-    let(:quotes) { Quotes.new }
-    let(:random_quote) { quotes.random_quote }
+describe Phrases do
+  let(:phrases) { Phrases.new }
+  let(:random_phrase) { phrases.random_phrase }
+  let(:json) { phrases.make_request }
 
-    it 'should return a hash object' do
-      expect(random_quote.class).to eql(Hash)
+  describe '#random_phrase' do
+    it 'should return a a type object hash' do
+      expect(random_phrase.class).to eql(Hash)
     end
 
     it 'should return a hash object' do
-      expect(random_quote.class).not_to eql(Array)
+      expect(random_phrase.class).not_to eql(String)
     end
   end
 
   describe '#make_request' do
-    let(:request) { Quotes.new }
-    let(:json) { request.make_request }
+    it 'returns a non nill element of JSON object as the json should be filled' do
+      expect(json[0]).not_to eql(nil)
+    end
 
+    it 'returns true as there is one element inside the json object array' do
+      expect(json.length >= 1).to eql(true)
+    end
     it 'should return a JSON object' do
-      expect(json.class).to eql(Array)
+      expect(json.instance_of?(Array)).to be true
     end
 
     it 'should not return a JSON hash' do
       expect(json.class).not_to eql(Hash)
-    end
-
-    it 'returns a non-empty JSON object' do
-      expect(json.length).not_to eql(0)
-    end
-
-    it 'returns a false' do
-      expect(json.length == 1).to eql(false)
     end
   end
 end
